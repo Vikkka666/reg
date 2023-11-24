da5a5309bdc85d02e17b6347354719546f811664
Vikkka666 <136796446+Vikkka666@users.noreply.github.com>
2023-11-24 18:01:39 +0500

    reg.js

--- reg.js
+++ reg.js
@@ -1,7 +1,55 @@
-let schet=0;
-let name=document.querySelector('#name');
-let email=document.querySelector('#email');
-let password=document.querySelector('#password');
-names=/^[a-zA-Z]{1,20}$/;
-emails=/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-passwords=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+const usernameRegex = /^[a-zA-Z]{1,20}$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+
+function nameShowResult(elementId, isValid) {
+  const resultElement = document.getElementById(elementId);
+  if (isValid) {
+    resultElement.textContent = "Готово";
+    resultElement.style.color = "green";
+  } else {
+    resultElement.textContent = "Имя должно состоять только из латинских букв от 1 до 20 символов";
+    resultElement.style.color = "red";
+  }
+}
+
+
+
+function emailShowResult(elementId, isValid) {
+    const resultElement = document.getElementById(elementId);
+    if (isValid) {
+      resultElement.textContent = "Готово";
+      resultElement.style.color = "green";
+    } else {
+      resultElement.textContent = "Неправильно набран email";
+      resultElement.style.color = "red";
+    }
+  }
+
+
+
+  function passwordShowResult(elementId, isValid) {
+    const resultElement = document.getElementById(elementId);
+    if (isValid) {
+      resultElement.textContent = "Готово";
+      resultElement.style.color = "green";
+    } else {
+      resultElement.textContent = "Не подходящий пароль";
+      resultElement.style.color = "red";
+    }
+  }
+
+
+document.getElementById("button").addEventListener("click", function() {
+  const usernameInput = document.getElementById("name");
+  const username = usernameInput.value.trim();
+  const useremailInput = document.getElementById("email");
+  const useremail = useremailInput.value.trim();
+
+
+  nameShowResult("p_name", usernameRegex.test(username));
+  emailShowResult("p_email", emailRegex.test(useremail));
+
+});
